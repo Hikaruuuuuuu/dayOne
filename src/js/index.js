d425eeb1cfcff46d0528b45631c21cb48041b497
Hikaru <229595976@qq.com>
2018-12-25 20:16:09 +0800

    bscroll

--- src/js/index.js
+++ src/js/index.js
@@ -5,37 +5,44 @@ function Show(){
 Show.prototype.init = function(){
 
     this.drawing()
-    this.ajax()
-
+    this.bscroll()
 }
 
 Show.prototype.drawing = function(){
+
+    // swiper
     var slide = document.getElementsByClassName('swiper-slide'),
         that = this;
-
-    this.ajax(shy)
-
+    this.ajax('./data/data.json',shy)
     function shy(data){
-        console.log()
         Array.from(slide).forEach((file,index)=>{
-            console.log(file,index)
             var html = "";
             data.data.primary_filter.slice(index*10,(index + 1) * 10).forEach((item)=>{
                 html += `<dl><dt><img src=${item.url}></dt><dd>${item.name}</dd></dl>`
             })
             file.innerHTML = html;
-            console.log(html)
             that.swiper()
+        }) 
+    }
+
+    // somthing bscroll
+    var tourContainer = document.getElementsByClassName('tour-container')[0];
+    this.ajax('./data/somthing.json',scrolls)
+    function scrolls(data){
+        var html = "";
+        data.data.poilist.forEach((file)=>{
+            console.log()
+            html += `<dl class="som"><dt><img src=${file.pic_url}></dt><dd>${file.name}</dd></dl>`
         })
-        
+        tourContainer.innerHTML = html;
     }
 
 }
 
-Show.prototype.ajax = function(ck){
+Show.prototype.ajax = function(url,ck){
     
     var xhr = new XMLHttpRequest(ck);
-        xhr.open('get','../data/data.json')
+        xhr.open('get',url)
         xhr.send()
         xhr.onreadystatechange = function(){
             if(xhr.readyState==4 && xhr.status == 200){
@@ -45,14 +52,15 @@ Show.prototype.ajax = function(ck){
         }
 }
 
-Show.prototype.swiper = function(){
-    
+Show.prototype.swiper = function(){ 
     var swipers = new Swiper('.swiper-container',{
         pagination: {
             el: '.swiper-pagination'
         }
     })
+}
 
-    
-
+Show.prototype.bscroll = function(){
+    var scrollOne = new BScroll('.main')
+   
 }
